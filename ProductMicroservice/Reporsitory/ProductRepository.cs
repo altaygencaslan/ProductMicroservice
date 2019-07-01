cabdccae17da616cee6a858c1e9b0bef41e677be
altaygencaslsan <wK9R2H6uvFE7s2rMYQg6>
2019-07-02 00:32:55 +0300

    Add project files.

---
+++ ProductMicroservice/Reporsitory/ProductRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProductMicroservice.DbContexts;
+using ProductMicroservice.Model;
+
+namespace ProductMicroservice.Reporsitory
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductContext _dbContext;
+        public ProductRepository(ProductContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void DeleteProduct(int ProductId)
+        {
+            var product = _dbContext.Products.Find(ProductId);
+            _dbContext.Products.Remove(product);
+            Save();
+        }
+
+        public Product GetProductById(int ProductId)
+        {
+            return _dbContext.Products.Find(ProductId);
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _dbContext.Products.AsEnumerable();
+        }
+
+        public void InsertProduct(Product product)
+        {
+            _dbContext.Products.Add(product);
+            Save();
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _dbContext.Entry(product).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            Save();
+        }
+    }
+}
